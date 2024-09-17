5c05b252f791deb609d8f81c88e625c44f82f50d
s poorna chandra <poornanaidu1@gmail.com>
2024-09-18 04:05:39 +0530

    changes

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -11,6 +11,8 @@ output "ami" {
 
 resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.id
-  instance_type = "t3.small"
+  instance_type = var.instance_type
   vpc_security_group_ids = ["sg-0c9e1ed0e2e200466"]
 }
+
+variable "instance_type" {}
