1cd4976e7ea032ff124307ece16cba5021dcac7c
s poorna chandra <poornanaidu1@gmail.com>
2024-09-18 03:52:26 +0530

    changes

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,9 +1,16 @@
-data "aws_ami_ids" "ami" {
-  name_regex = "centos-*"
+data "aws_ami_id" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
   owners = ["973714476881"]
 }
 
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami_id.ami
+}
+
+resource "aws_instance" "instance" {
+  ami = data.aws_ami_id.ami
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0c9e1ed0e2e200466"]
 }
