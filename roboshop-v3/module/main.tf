97a65336823bbdba831b69813d8f0d19f54bde91
s poorna chandra <poornanaidu1@gmail.com>
2024-09-18 11:27:54 +0530

    changes

---
+++ roboshop-v3/module/main.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
+
+
+output "ami" {
+  value = data.aws_ami.ami
+}
+
+resource "aws_instance" "instance" {
+  ami = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = ["sg-0c9e1ed0e2e200466"]
+}
+
+variable "instance_type" {}
