c9107bca4cb397b3341d2d645aa97cb505b40313
s poorna chandra <poornanaidu1@gmail.com>
2024-11-21 12:00:02 +0530

    terraform

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,11 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+provider "aws" {
+  region = "us-east-1" # Replace with your desired AWS region
+  profile = "default" # Specify the AWS CLI profile to use (optional)
 }
+
 
 
 resource "aws_instance" "frontend" {
