31bfcc914b9ae24ef59e3a0d1a4cbcf539ff180e
s poorna chandra <poornanaidu1@gmail.com>
2024-11-21 12:16:42 +0530

    terraform

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -10,6 +10,7 @@ terraform {
 
 resource "aws_instance" "frontend" {
   ami           = "ami-0b4f379183e5706b9"
+  region        = "us-east-1"
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-02f295a7bd0d62dde" ]
 
@@ -18,6 +19,3 @@ resource "aws_instance" "frontend" {
   }
 }
 
-provider "aws" {
-  region = "us-east-1" # Replace with your desired AWS region# Specify the AWS CLI profile to use (optional)
-}
