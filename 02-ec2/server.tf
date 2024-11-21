01060ffd14fac873de9eb2a30487c2f86db338b1
s poorna chandra <poornanaidu1@gmail.com>
2024-11-21 12:20:35 +0530

    terraform

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,21 +1,44 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b4f379183e5706b9"
-  region        = "us-east-1"
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-02f295a7bd0d62dde" ]
+  vpc_security_group_ids = [ "sg-0577c0c4fa7613b87" ]
 
   tags = {
     Name = "frontend"
   }
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z095093821BU1FHHF63BW"
+  name    = "frontend-dev.poornadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+  terraform {
+    backend "s3" {
+      bucket = "tf-state-poorna"
+      key    = "ami/terraform.tfstate"
+      region = "us-east-1"
+    }
+  }
+
+  data "aws_ami" "ami" {
+    most_recent = true
+    name_regex  = "Centos-8-DevOps-Practice"
+    owners      = ["973714476881"]
+  }
+
+  data "aws_security_group" "sg" {
+    name = "allow-all"
+  }
+
+  resource "aws_instance" "ami" {
+    instance_type = "t3.small"
+    ami = data.aws_ami.ami.image_id
+    vpc_security_group_ids = [data.aws_security_group.sg.id]
+    tags= {
+      Name = "ami"
+    }
+  }
 
