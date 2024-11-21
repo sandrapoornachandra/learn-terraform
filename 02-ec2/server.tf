752a13f6f398b0024358fd33067b74bc194e10c7
s poorna chandra <poornanaidu1@gmail.com>
2024-11-21 11:53:27 +0530

    terraform

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 
 resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9"
